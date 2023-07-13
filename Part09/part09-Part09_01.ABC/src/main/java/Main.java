4923cce8549f33a7a531e281ea1939eb12a1d091
IGoToSchoolByBus <70379183+YccYeung@users.noreply.github.com>
2023-07-12 23:59:03 -0500

    Add files via upload

---
+++ Part09/part09-Part09_01.ABC/src/main/java/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
+        C c = new C();
+
+        a.a();
+        c.a();
+        b.a();
+        c.b();
+        
+    }
+}
